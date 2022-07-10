4ca18fad105d7070a55f20f3ae508c220b9265d4
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-07-10 18:54:08 -0300

    Refazendo o CSS para mobile-frist| Menu terminado

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,17 @@
-document.querySelector('.menu-abrir').onclick = () => {
-    document.documentElement.classList.add('menu-ativo')
-}
+const $ = document.querySelector.bind(document)
+const botaoAbrir = $('.menu-abrir')
+const botaoFechar = $('.menu-fechar')
 
-document.querySelector('.menu-fechar').onclick = e => {
+document.querySelector('.menu-abrir').onclick = () => {
+    document.documentElement.classList.add('menu-ativo');
+};
+
+document.querySelector('.menu-fechar').onclick = () => {
+    document.documentElement.classList.remove('menu-ativo');
+};
+
+document.documentElement.onclick = e => {
     if(e.target === document.documentElement) {
         document.documentElement.classList.remove('menu-ativo')
     }
-}
+};
